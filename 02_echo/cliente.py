62250ded69c550aff6f80a5f46ccddf866dc0e86
axlcraft <axldaniel10@gmail.com>
2025-08-04 23:19:05 -0500

    new new

--- 02_echo/cliente.py
+++ 02_echo/cliente.py
@@ -1,2 +1,20 @@
 # Cliente
+import socket
+
+HOST = 'localhost' 
+PORT = 9000 
+
+mensaje = input("Digite su mensaje: el agua moja")
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+cliente.connect((HOST, PORT))
+
+cliente.sendall(mensaje.encode()) 
+print(f"Mensaje enviado: '{mensaje}'")
+
+respuesta = cliente.recv(1024)
+print(f"Respuesta del 'Echo': '{respuesta.decode()}'") 
+
+cliente.close()
 
